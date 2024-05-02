8c16da08ed0e71571e68a44c2bf968c68e4097e2
João Parro <jpedroparro@gmail.com>
2024-05-02 11:08:55 -0300

    Login FireBase
    apenas autenticação de Email

--- app/src/main/java/br/udesc/drinkappddm/MainActivity.kt
+++ app/src/main/java/br/udesc/drinkappddm/MainActivity.kt
@@ -1,57 +1,60 @@
 package br.udesc.drinkappddm
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.udesc.drinkappddm.databinding.ActivityMainBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
+    private lateinit var auth: FirebaseAuth //autenticação para o FireBASe - J.parro
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding?.root)
 
-        setSupportActionBar(binding.appBarMain.toolbar)
+        auth = Firebase.auth//J.Parro
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        binding?.btnLogin?.setOnClickListener{//RECUPERANDO OS DADOS DA TELA DE LOGIN
+            val email: String = binding?.etEmail?.text.toString()
+            val password: String = binding?.etPassword?.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty()){
+                signInWithEmailAndPassword(email,password)
+            }else {
+                Toast.makeText(this@MainActivity,"Por favor, preencha os campos",Toast.LENGTH_SHORT).show()
+            }
         }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+        binding?.tvCreateAccount?.setOnClickListener {
+            val intent = Intent(this@MainActivity, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
+    private fun signInWithEmailAndPassword(email: String, password: String){
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                Log.d(TAG,"signInUserWithEmailAndPassWord")
+                //val user = auth.currentUser
+                Toast.makeText(baseContext, "Login Sucess", Toast.LENGTH_SHORT).show()
+            }else {
+                Log.w(TAG, "signInUserWithEmailAndPassaword:Failure", task.exception)
+                Toast.makeText(baseContext, "Authentication Failure", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    companion object{
+        private var TAG = "EmailAndPassaWord"
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
---
+++ app/src/main/java/br/udesc/drinkappddm/RegisterActivity.kt
@@ -0,0 +1,61 @@
+package br.udesc.drinkappddm
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import br.udesc.drinkappddm.databinding.ActivityRegisterBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
+class RegisterActivity : AppCompatActivity(){
+    private lateinit var auth: FirebaseAuth
+    private var binding: ActivityRegisterBinding?= null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        auth = Firebase.auth
+
+        binding?.btnRegister?.setOnClickListener{
+            val email: String = binding?.etEmail?.text.toString()
+            val password: String = binding?.etPassword?.text.toString()
+            val confirmPassword: String = binding?.etConfirmPassword?.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()){
+                if (password == confirmPassword){
+                    createUserWithEmailAndPassaword(email,password)
+                }else{
+                    Toast.makeText(this@RegisterActivity,"Senha incompativel",Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        }
+
+    }
+    private fun createUserWithEmailAndPassaword(email: String, password: String){
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+            if (task.isSuccessful){
+                Log.d(TAG,"createUserWithEmailAndPassaword:Sucess")
+                val user = auth.currentUser
+                Toast.makeText(baseContext, "Authentication Sucess", Toast.LENGTH_SHORT).show()
+
+            }else{
+                Log.w(TAG,"createUserWithEmailAndPassaword:Failure",task.exception)
+            Toast.makeText(baseContext, "Authentication Failure", Toast.LENGTH_SHORT).show()
+        }}
+    }
+
+        companion object{
+            private var TAG = "EmailAndPassaWord"
+        }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+
+}
