4105fb18c0cdfbd6ed4480dc4c635c8eb0ca0c8a
tobiasfkk <78160146+tobiasfkk@users.noreply.github.com>
2024-05-19 14:50:10 -0300

    ajustado listagem dos produtos

--- app/src/main/java/br/udesc/drinkappddm/repository/ProdutoRepository.kt
+++ app/src/main/java/br/udesc/drinkappddm/repository/ProdutoRepository.kt
@@ -5,36 +5,34 @@ import br.udesc.drinkappddm.Model.Produto
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
 
-
-class ProdutoRepository() {
+class ProdutoRepository {
     private val db = FirebaseFirestore.getInstance()
     private val produtosCollection = db.collection("produtos")
-    private val categoriasCollection = db.collection("categorias")
 
     suspend fun addProduto(produto: Produto) {
         db.collection("produtos")
             .add(produto)
             .await()
     }
+
     suspend fun obterCategorias(): List<Categoria> {
         return try {
             val categoriasSnapshot = db.collection("categorias").get().await()
             val categoriasList = mutableListOf<Categoria>()
             for (categoriaSnapshot in categoriasSnapshot.documents) {
-
                 val nomeCategoria = categoriaSnapshot.getString("nome") ?: continue
                 categoriasList.add(Categoria(nomeCategoria))
             }
             categoriasList
         } catch (e: Exception) {
-            // Lidar com erros de obtenção das categorias, por exemplo, lançar uma exceção ou retornar uma lista vazia
+            // Lidar com erros de obtenção das categorias
             emptyList()
         }
     }
 
     suspend fun obterProdutosPorCategoria(categoria: Categoria): List<Produto> {
         return try {
-            val produtosSnapshot = produtosCollection.whereEqualTo("categoria", categoria.nome).get().await()
+            val produtosSnapshot = produtosCollection.whereEqualTo("categoria.nome", categoria.nome).get().await()
             val produtosList = mutableListOf<Produto>()
             for (produtoSnapshot in produtosSnapshot.documents) {
                 val nome = produtoSnapshot.getString("nome") ?: continue
@@ -44,7 +42,7 @@ class ProdutoRepository() {
             }
             produtosList
         } catch (e: Exception) {
-            // Lidar com erros de obtenção dos produtos, por exemplo, lançar uma exceção ou retornar uma lista vazia
+            // Lidar com erros de obtenção dos produtos
             emptyList()
         }
     }
