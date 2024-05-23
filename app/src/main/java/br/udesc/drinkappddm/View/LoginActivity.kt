9a6f63ae86659429b1dd83454652225521bc9329
elianBencke <168542697+elianBencke@users.noreply.github.com>
2024-05-23 00:51:45 -0300

    criação de gerenciador

--- app/src/main/java/br/udesc/drinkappddm/View/LoginActivity.kt
+++ app/src/main/java/br/udesc/drinkappddm/View/LoginActivity.kt
@@ -44,8 +44,8 @@ class LoginActivity : AppCompatActivity() {
     private fun startActivity() {
 //        val intent = Intent(this, CatalogoCategoriaActivity::class.java)
 //        val intent = Intent(this, PagamentoActivity::class.java)
-        val intent = Intent(this, ProdutoActivity::class.java)
-
+        //val intent = Intent(this, ProdutoActivity::class.java)
+        val intent = Intent(this, GerenciarProdutoActivity::class.java)
         startActivity(intent)
         //finish() //  eencerra a tela
     }
